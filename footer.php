31474b8b4644d7fb9529745869140d37492b4d53
clguitar1 <clguitar1@gmail.com>
2017-04-16 21:07:34 -0400

    First commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wordpress_Starter_Kit
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wordpressstarterkit' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wordpressstarterkit' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wordpressstarterkit' ), 'wordpressstarterkit', '<a href="https://automattic.com/" rel="designer">CL Digital Design</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
